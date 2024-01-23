1156412900511a0378f481d9c08da0a3ddc1801e
patrictobola <tobola92@gmail.com>
2024-01-23 17:06:02 +0100

    done Api calls and a simple pagination

---
+++ js/script.js
@@ -0,0 +1,66 @@
+const apiKey = 'ef4b6b74657f4288a4a154605242301';
+const cities = ['London', 'Milan', 'Bangkok', 'Los Angeles', 'Nairobi'];
+let currentIndex = 0;
+
+function getDayOfWeek(dateString) {
+    const daysOfWeek = ['Dom', 'Lun', 'Mar', 'Mer', 'Gio', 'Ven', 'Sab'];
+
+    const date = new Date(dateString);
+    const dayIndex = date.getDay();
+
+    return daysOfWeek[dayIndex];
+}
+
+function getWeather(city) {
+    const apiUrl = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=7&aqi=no&alerts=no`;
+    return $.get(apiUrl);
+}
+
+function updateWidget(city) {
+    getWeather(city).done((data) => {
+        const { location, current, forecast } = data;
+        const temperature = `${current.temp_c}°C`;
+        const iconUrl = `http:${current.condition.icon}`;
+        
+        $('.city-name').text(location.name);
+        $('.temperature').text(temperature);
+        $('.weather-icon').attr('src', iconUrl);
+
+        const forecastHtml = forecast.forecastday.map(day => `
+            <div class="forecast-item">
+            <div>${getDayOfWeek(day.date)}</div>
+                <div>Min: ${day.day.mintemp_c}°C</div>
+                <div>Max: ${day.day.maxtemp_c}°C</div>
+            </div>
+        `).join('');
+
+        $('.carousel').html(forecastHtml);
+    });
+}
+
+function updatePagination() {
+    const totalCities = cities.length;
+    $('.prev').prop('disabled', currentIndex === 0);
+    $('.next').prop('disabled', currentIndex === totalCities - 1);
+}
+
+$(document).ready(() => {
+    updateWidget(cities[currentIndex]);
+
+    $('.prev').click(() => {
+        if (currentIndex > 0) {
+            currentIndex--;
+            updateWidget(cities[currentIndex]);
+            updatePagination();
+        }
+    });
+
+    $('.next').click(() => {
+        const totalCities = cities.length;
+        if (currentIndex < totalCities - 1) {
+            currentIndex++;
+            updateWidget(cities[currentIndex]);
+            updatePagination();
+        }
+    });
+});
